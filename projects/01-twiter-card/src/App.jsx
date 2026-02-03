b8b3b560af8343e22e0e154cef1aa365bf0fa0e1
VictorRodBon <victorrodbon@gmail.com>
2026-02-03 17:52:58 +0100

    primera clase

--- projects/01-twiter-card/src/App.jsx
+++ projects/01-twiter-card/src/App.jsx
@@ -5,21 +5,56 @@ export function App(){
 
     const formatUserName=(username)=>`@${username}`;
 
+    // creamos un objeto JSON con los datos y se los pasamso al componente (no es muy buena práctica)
+    const victor={formatUserName: formatUserName,username:"Victor_Rod_Bonn", name:"Victor Rodriguez Bon"};
+
+    const users=[
+        {
+            username:"midudev",
+            name: "Miguel Ángel Durán",
+            isFollowing:true
+        },
+        {
+            username:"pheralb",
+            name: "Pablo H.",
+            isFollowing:false
+        },
+        {
+            username:"PacoHdzs",
+            name: "Paco Hdez.",
+            isFollowing:true
+        }
+    ]
+
     return(
         <section className="App">
             <TwiterFollowCard 
-                isFollowing 
                 formatUserName={formatUserName} 
                 username="midudev" 
                 name="Miguel Ángel Durán" 
+                initialIsFollowing={true}
             />
 
             <TwiterFollowCard 
-                isFollowing={false} 
-                formatUserName={formatUserName} 
-                username="Victor_Rod_Bonn" 
-                name="Victor Rodriguez Bon"
+                {...victor} // convertimos las propiedades del objeto props del componente
             />
+
+            {/* Renderizamos un array */}
+            {/* Indicamos un identificador único en el renderizado */}
+            {
+                users.map(user=>{
+                    const {username, name, isFollowing}=user;
+                    return(
+                        <TwiterFollowCard 
+                            key={userName}
+                            formatUserName={formatUserName}
+                            username={username}
+                            initialIsFollowing={isFollowing}
+                            name={name}
+                        />
+                    )
+                })
+            }
         </section>
     )
 }  
--- projects/01-twiter-card/src/TwiterFollowCard.jsx
+++ projects/01-twiter-card/src/TwiterFollowCard.jsx
@@ -1,13 +1,36 @@
-import "./App.css";
-export function TwiterFollowCard({formatUserName,name, username, isFollowing}){
+import { useState } from "react";
 
-    console.log(isFollowing)
+export function TwiterFollowCard({formatUserName,name, username, initialIsFollowing}){ 
+    // children para pasar como parámetro lo que pongamos dentro del componente (slot de Astro)
+    // los por defecto es como en una función en JS
+    
+    const state=useState(initialIsFollowing);
+    const isFollowing=state[0]; // => valor del estado
+    const setIsFollowing = state[1]; // => función aftualizar estado
+    
+    // const [isFollowing, setIsFollowing] = userState(false) // JS indica que la primera posición equivale a la primera constante y el segundo a la segunda
 
+    const text = isFollowing 
+        ? 'Siguiendo' 
+        : 'Seguir'
+
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
+
+    // damos la vuelta al valor del estado
+    const handleClick=()=>{
+        setIsFollowing(!isFollowing);
+    }
 
     return(
         <article className="tw-followCard">
             <header className="tw-followCard-header">
-                <img className="tw-followCard-avatar" src={`https://unavatar.io/${username}`} alt="el avatar de midudev" />
+                <img 
+                    className="tw-followCard-avatar" 
+                    src={`https://unavatar.io/${username}`} 
+                    alt="el avatar de midudev" 
+                />
                 <div className="tw-followCard-info">
                     <strong>{name}</strong>
                     <span className="tw-followCard-infoUserName">{formatUserName(username)}</span>              
@@ -15,8 +38,9 @@ export function TwiterFollowCard({formatUserName,name, username, isFollowing}){
             </header>
 
             <aside>
-                <button className="tw-followCard-button">
-                    seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
