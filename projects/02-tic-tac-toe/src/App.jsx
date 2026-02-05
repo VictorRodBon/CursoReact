77898c5e07f78047affcd17e67cd4bd08f01c4fd
VictorRodBon <victorrodbon@gmail.com>
2026-02-05 11:46:13 +0100

    Guardar estado de la partida en el local storage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -7,9 +7,19 @@ import { Square } from './components/Square.jsx'
 import { TURNS, WINNER_COMBOS } from './constants.js';
 
 function App() {
-  const [board,setBoard]=useState(Array(9).fill(null));
+
+
+  const [board,setBoard]=useState(()=>{
+    const boardFromStorage=window.localStorage.getItem('board');
+    
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
   
-  const [turn, setTurn]=useState(TURNS.X);
+  const [turn, setTurn]=useState(()=>{
+    const turnFromStorage=window.localStorage.getItem('turn');
+
+    return turnFromStorage ? JSON.parse(turnFromStorage) :  TURNS.X
+  });
 
   const[winner,setWinner]=useState(null)
 
@@ -31,6 +41,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board');
+    window.localStorage.removeItem('turn');
   }
 
   const checkEndGame =(newBoard)=>{
@@ -47,6 +60,11 @@ function App() {
     const newTurn= turn===TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
+    // guardar estado partida
+    window,localStorage.setItem('board', JSON.stringify(newBoard));
+    window,localStorage.setItem('turn', JSON.stringify(newTurn));
+
+    // comprobar ganador
     const newWinner=checkWinner(newBoard);
     if(newWinner){
       confetti()
